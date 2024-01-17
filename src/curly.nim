22190f2bf7c6f63d01348a1adf0a193c7cb07ca8
Ryan Oldenburg <ryan@guzba.com>
2024-01-17 11:41:12 -0600

    ?

--- src/curly.nim
+++ src/curly.nim
@@ -1,5 +1,6 @@
-when not defined(gcArc) and not defined(gcOrc):
-  {.error: "Using --mm:arc or --mm:orc is required by Curly.".}
+when not defined(nimdoc):
+  when not defined(gcArc) and not defined(gcOrc):
+    {.error: "Using --mm:arc or --mm:orc is required by Curly.".}
 
 when not compileOption("threads"):
   {.error: "Using --threads:on is required by Curly.".}
